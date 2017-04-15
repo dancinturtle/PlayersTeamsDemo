03bd1baebe95e82bebfe082792e65f1753976a1f
dancinturtle <amy.giver@gmail.com>
2017-04-15 12:47:44 -0700

    initial commit

---
+++ server.js
@@ -0,0 +1,10 @@
+var express = require('express'),
+  path = require('path'),
+  app = express();
+
+app.use(express.static(path.join(__dirname, "./client")));
+app.use(express.static(path.join(__dirname, "./bower_components")));
+
+app.listen(8000, function(){
+  console.log("Listening on port 8000");
+})
